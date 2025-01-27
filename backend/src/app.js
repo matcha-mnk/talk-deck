336ba57b380979d23be1c489d344a995b0e75c77
matcha-mnk <noiha1161@gmail.com>
2025-01-27 21:55:43 +0900

    環境構築

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/backend', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
