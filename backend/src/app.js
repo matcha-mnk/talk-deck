0d1c46ffc2f57ef43e43e49e95501ab4064c6192
matcha-mnk <noiha1161@gmail.com>
2025-01-31 10:51:13 +0900

    Renderのurlでfrontendに行くように

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,11 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 app.get('/backend', (req, res) => {
   res.json({ message: 'Hello from backend!' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 app.listen(PORT, () => {
