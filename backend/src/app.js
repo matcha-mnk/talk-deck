d98c3268271efaddbdc88ce5d1ccc7dd583a92fb
matcha-mnk <noiha1161@gmail.com>
2025-01-31 10:54:09 +0900

    パス修正

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 // app.use(express.json());
 app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
+app.use(express.static(path.join(__dirname, '../frontend')))
 
 app.get('/backend', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(PORT, () => {
