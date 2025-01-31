6265a97a5bbdf21a5a59d6c7155fbbc9bb69d0fc
matcha-mnk <noiha1161@gmail.com>
2025-01-31 10:17:50 +0900

    config修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import dotenv from 'dotenv'
-
-dotenv.config();
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: require.resolve('react'),
+    },
+  },
   server: {
     proxy: process.env.NODE_ENV === 'development' ? {
       '/backend': {
