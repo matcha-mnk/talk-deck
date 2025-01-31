0fa86b45a569aa105a5e108294f5a7942bb18217
matcha-mnk <noiha1161@gmail.com>
2025-01-31 10:06:31 +0900

    不要なlogを削除

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +13,8 @@ app.get('/backend', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
+/*
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+*/
